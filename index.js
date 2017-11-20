0bcd5df25c704ca18b321c7e7fab3579cb4f0592
Nurmik <33057046+Nurmik@users.noreply.github.com>
2017-11-20 13:50:45 +0200

    1

---
+++ index.js
@@ -0,0 +1,43 @@
+ 
+
+ /*
+ 1.ID
+ 2.Games owned
+ 3.Hours played
+ 4.Profile created
+ */
+
+users = [
+{
+ name: 'Yeti', //char+
+ gamesowned: '47', //int
+ hoursplayed: '2332', //float
+ profilecreated: '4 years ago'
+},
+{
+name: 'Doggolord', //char+
+ gamesowned: '1678', //int
+ hoursplayed: '2829.8', //float
+ profilecreated: '7 years ago'
+},
+{
+name: 'Pinkie', //char+
+ gamesowned: '28', //int
+ hoursplayed: '945', //float
+ profilecreated: '2 years ago'
+},
+{
+name: 'Pinkie2', //char+
+ gamesowned: '28', //int
+ hoursplayed: '945', //float
+ profilecreated: '2 years ago'
+}
+]
+
+userStats = user => `
+	<h1> ${user.name} </h1>
+	<p> Hours played: ${user.hoursplayed} </p>
+`
+
+document.querySelector('body').innerHTML = users.map(userStats)
+	
