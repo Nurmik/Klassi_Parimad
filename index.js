425c28a561b499b387237ba3445dc72974a720d6
Nurmik <33057046+Nurmik@users.noreply.github.com>
2017-11-21 13:39:45 +0200

    ....

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
  
-
  /*
  1.ID
  2.Games owned
@@ -7,6 +6,53 @@
  4.Profile created
  */
 
+parseData = async data => {
+    return data.feed.entry.map(entry => {
+        return Object.keys(entry)
+            .map(field => {
+                if (field.startsWith('gsx$')) {
+                    return [
+                        field.split('$')[1],
+                        entry[field].$t
+                    ]
+                }
+            })
+            .filter(field => field)
+            .reduce((field, item) => {
+                field[item[0]] = item[1]
+                return field
+            }, {})
+    })
+}
+
+
+async function getData() {
+    const key = '1bx-X8xb1m26_4ik0pQ1KLhO1fOaEUqeiLAd-AEultWk'
+    const res = await fetch(`https://docs.google.com/spreadsheets/d/1bx-X8xb1m26_4ik0pQ1KLhO1fOaEUqeiLAd-AEultWk/edit#gid=0`);
+    const data = await res.json()
+    return await parseData(data)
+}
+
+userStats = user => `
+	<h1> ${user.name} </h1>
+	<p> Games owned: ${user.gamesowned} </p>
+	<p> Hours played: ${user.hoursplayed} </p>
+	<p> Profile created: ${user.profilecreated} </p>
+`
+getData().then(users => {
+    document.querySelector('body').innerHTML = users.map(userStats).join('')
+})
+
+
+
+
+
+
+
+
+
+
+/*
 users = [
 {
  name: 'Yeti', //char+
@@ -26,21 +72,4 @@ name: 'Pinkie', //char+
  hoursplayed: '945', //float
  profilecreated: '2 years ago'
 },
-{
-name: 'Pinkie2', //char+
- gamesowned: '28', //int
- hoursplayed: '945', //float
- profilecreated: '2 years ago'
-}
-]
-
-userStats = user => `
-	<h1> ${user.name} </h1>
-	<p> Games owned: ${user.gamesowned} </p>
-	<p> Hours played: ${user.hoursplayed} </p>
-	<p> Profile created: ${user.profilecreated} </p>
-
-`
-
-document.querySelector('body').innerHTML = users.map(userStats)
-	
+]	
